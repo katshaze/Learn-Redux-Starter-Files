b0fa2ca7b05d6fb01202419c8d198132630d35cb
katshaze <katrinamargaret.hayes@gmail.com>
2018-06-28 17:23:56 +1000

    feat: react scaffolding

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,12 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+// import css
+import css from './styles/style.styl';
+
+//import components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
+
+// if no path on the front of the location of where we're importing something, it means it's something
+// coming from node modules folder and somethign that we npm installed at the start.
