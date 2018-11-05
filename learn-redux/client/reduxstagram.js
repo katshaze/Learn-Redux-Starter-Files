7b1da0f56d80d217c8ecf62a5a244684fa73dd4e
katshaze <katrinamargaret.hayes@gmail.com>
2018-11-05 13:41:54 +1100

    feat: create components, setup router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {render} from 'react-dom';
+
+import { render } from 'react-dom';
+
 // import css
 import css from './styles/style.styl';
 
 //import components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
 
 // if no path on the front of the location of where we're importing something, it means it's something
 // coming from node modules folder and somethign that we npm installed at the start.
+
+// the render method from react-dom: render out to html
+
+// react router: render out the router to the page and then it will decide which componenent needs to be rendered
