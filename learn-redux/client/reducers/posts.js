ad0acbe1a2aca2058ac3804bb65c3a9e816b07e1
katshaze <katrinamargaret.hayes@gmail.com>
2018-11-12 17:07:30 +1100

    feat: setting up reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,13 @@
+// a reducer takes in two things:
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+// the reducer has a look, does some stuff, and returns a new updated store
+
+// first time this runs, there won't be any state, so need to start the state as an empty array
+function posts(state=[], action) {
+    console.log(state, action);
+    return state;    
+}
+
+export default posts;
